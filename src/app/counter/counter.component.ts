73eab81a45a2947b23ca8d7174b0ed3612ab6826
borishdez <borisnandez93@gmail.com>
2024-06-06 16:07:14 -0600

    counter component creado

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  //nombre de componente
+  selector: 'app-counter',
+  template: '<h1>Hola Mundo!</h1>',
+})
+export class CounterComponent {
+
+}
