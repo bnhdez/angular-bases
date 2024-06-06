4f71a28019c2c3b0a98e153ee8057a05554ff9ac
borishdez <borisnandez93@gmail.com>
2024-06-06 15:48:47 -0600

    contador de value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
   public title: string = 'Mi primera App de Angular';
   public counter: number = 10;
 
-  increaseBy(): void{
-    this.counter += 1;
+  increaseBy(value:number): void{
+    this.counter += value;
   }
+
 }
