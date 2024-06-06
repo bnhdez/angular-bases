9494fcad84cfcb8f06bc2e5208ad38b79f2128f3
borishdez <borisnandez93@gmail.com>
2024-06-06 16:12:34 -0600

    app-counter importado

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
 @Component({
   //nombre de componente
   selector: 'app-counter',
-  template: '<h1>Hola Mundo!</h1>',
+  template: '<h1>Hola Counter</h1>',
 })
 export class CounterComponent {
 
