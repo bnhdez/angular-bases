c7bf85dd9eade94b672ac59c3d84d553b4732fc4
borishdez <borisnandez93@gmail.com>
2024-06-07 12:58:47 -0600

    metodos changeHero y changeAge implementados

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -21,4 +21,12 @@ export class HeroComponent {
     return `${this.name} - ${this.age}`;
   }
 
+  changeHero(): void {
+    this.name = 'Spider-Man';
+  }
+
+  changeAge(): void {
+    this.age = 25
+  }
+
 }
