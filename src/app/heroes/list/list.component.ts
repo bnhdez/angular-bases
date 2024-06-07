a95d236ad3dc3ddf305f91e6ad879e7507393cab
borishdez <borisnandez93@gmail.com>
2024-06-07 14:20:07 -0600

    deletedHero funcional

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
 export class ListComponent {
 
   public heroNames:string[] = ['Spider-Man', 'Iron-Man', 'Hulk', 'She-Hulk', 'Thor'];
+  public deletedHero?:string;
+
+  // removeLastHero():void {
+  //   deleted hero solo existe dentro de removeLastHero()
+  //   const deletedhero = this.heroNames.pop();
+  // }
+
+  removeLastHero(): void {
+    //.pop() Remueve ulitmo elemento de la lista y lo regresa
+    this.deletedHero = this.heroNames.pop();
+  }
 
 }
