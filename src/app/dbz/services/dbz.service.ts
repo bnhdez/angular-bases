ea2aa7858225f5b64567dc52154eff1b38eaeb12
borishdez <borisnandez93@gmail.com>
2024-06-11 17:03:39 -0600

    id agregado a objetos

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -5,12 +5,15 @@ import { Character } from '../interfaces/character.interface';
 @Injectable({providedIn: 'root'})
 export class DbzService {
   public characters: Character[] = [{
+    id: uuid(),
     name: 'Krillin',
     power: 1000
   }, {
+    id: uuid(),
     name: 'Goku',
     power: 9500
   }, {
+    id: uuid(),
     name: 'Vegeta',
     power: 7500
   }];
@@ -19,8 +22,18 @@ export class DbzService {
     // console.log('Main Page');
     // console.log(character);
 
+    // const newCharacter:Character = {
+    //   id: uuid(),
+    //   name: character.name,
+    //   power: character.power
+    // }
+
+    //con ...character hago spread de las propiedades del objeto
+    //que traiga todo adentro de character
+    const newCharacter:Character = { id:uuid(), ...character }
+
     //push al final del arreglo || unshift al inicio del arreglo
-    this.characters.push(character);
+    this.characters.push(newCharacter);
   }
 
   onDeleteCharacter(index: number): void {
